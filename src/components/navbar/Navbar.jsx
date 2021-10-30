ea315d346e4e48bd72faef3b40cc23452bede299
islemwilcher <islemadobe6@gmail.com>
2021-10-30 18:22:46 +0100

    navbar desktop ui done

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,18 +1,22 @@
 
 import styled from 'styled-components'
-import {FiSearch} from 'react-icons/fi'
+
+import { FiSearch } from 'react-icons/fi'
+import { IoMdCart } from 'react-icons/io'
+
+import Badge from '@material-ui/core/badge'
 
 const Container = styled.div`
     height: 60px;
     background-color: whitesmoke;
-`;
+`
 
 const Wrapper = styled.div`
     padding: 10px 20px;
     display:flex;
     justify-content: space-between;
     align-items: center;
-`;
+`
 
 // left side
 const Left = styled.div`
@@ -21,11 +25,11 @@ const Left = styled.div`
     display: flex;
     justify-content: space-between;
     align-items: center;
-`;
+`
 const Language = styled.span`
     font-size: 14px;
     cursor: pointer;
-`;
+`
 const SearchContainer = styled.div`
     border: 1px solid lightgray;
     width: 90%;
@@ -33,24 +37,36 @@ const SearchContainer = styled.div`
     justify-content: space-between;
     padding: 3px 5px;
     align-items:center;
-`;
+`
 const Input = styled.input`
     border: none;
     width: 90%;
     padding: 5px;
-`;
+`
 
 //center side
 const Center = styled.div`
     padding: 10px;
     flex: 1;
-`;
+    text-align: center;
+`
+const Logo = styled.h1`
+    font-weight: bold;
+`
 
 //right side
 const Right = styled.div`
     padding: 10px;
     flex: 1;
-`;
+    display: flex;
+    justify-content: flex-end;
+`
+
+const MenuItem = styled.div`
+    font-size: 14px;
+    cursor: pointer;
+    margin-left: 25px;
+`
 
 const Navbar = () => {
     return (
@@ -60,11 +76,21 @@ const Navbar = () => {
                     <Language>EN</Language>
                     <SearchContainer>
                         <Input />
-                        <FiSearch />
+                        <FiSearch size='25px' />
                     </SearchContainer>
                 </Left>
-                <Center>center</Center>
-                <Right>right</Right>
+                <Center>
+                    <Logo>L0G0!</Logo>
+                </Center>
+                <Right>
+                    <MenuItem>REGESTER</MenuItem>
+                    <MenuItem>sign in</MenuItem>
+                    <MenuItem>
+                    <Badge badgeContent={0} color='primary'>
+                        <IoMdCart size='25px' />
+                    </Badge>
+                    </MenuItem>
+                </Right>
             </Wrapper>
         </Container>
     )
