6b1feb1e5742b28254ee663e9442e857dcc5f5d8
islemwilcher <islemadobe6@gmail.com>
2021-11-01 23:10:57 +0100

    adding the route to the app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 
-import Home from "./pages/Home";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
+
+import Category from "./pages/Category"
+import Home from "./pages/Home"
 
 
 function App() {
   return (
-    <div>
-      <Home />
-    </div>
+    <>
+      <Router>
+        <Switch>
+          <Route exact path='/' component={Home} />
+          <Route path='/category' component={Category} />
+        </Switch>
+      </Router>
+    </>
   );
 }
 
--- src/pages/Category.jsx
+++ src/pages/Category.jsx
@@ -1,8 +1,14 @@
 
+import Announcement from '../components/Announcement'
+import Navbar from '../components/Navbar'
+import Footer from '../components/Footer'
+
 const Category = () => {
     return (
         <div>
-            
+            <Announcement />
+            <Navbar />
+            <Footer />
         </div>
     )
 }
